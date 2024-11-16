f549b0ec5fc8e33e546da6fb411eb5c526100225
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-11-16 13:22:12 -0500

    Magnifique mommit

--- Assets/Scripts/CloudGenerator.cs
+++ Assets/Scripts/CloudGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Numerics
 
 public class CloudGenerator : MonoBehaviour
 {
@@ -62,5 +63,41 @@ public class CloudGenerator : MonoBehaviour
     void Start()
     {
         SpawnElectrons(1000);
+    }
+
+    Complex GetSlaterTypeOrbital(int, n, int l, int m, float r, float theta, float phi, float zeta)
+    {
+        return (AngularPart(l, m, theta, phi, zeta) * RadialPart(r * n * zeta))
+    }
+
+    Complex AngularPart(int l, int m, float theta, float phi, float zeta)
+    {
+        Complex exponentialPart = Complex.Exp(new Complex(0, m * phi))
+        
+        return (get_normalization_constant(zeta, n) * exponentialPart * P(n, l, Mathf.Cos(theta)))
+    }
+
+    float RadialPart(float r, int n, float zeta)
+    {
+        return (get_normalization_constant(zeta, n) * Mathf.Pow(r, n - 1) * Mathf.Exp(zeta * r * -1))
+    }
+
+    float P(int l, int m, float theta)
+    {
+        int sum_bound = ((l - Mathf.Abs(m)) / 2) + 1/4 * (Mathf.Pow(-1, l - Mathf.Abs(m)) - 1);
+        int p_sum = 0;
+        for (int k = 0; k < sum_bound; k++)
+        {
+            p_sum += Mathf.Pow(-1, k) * (Mathf.Factorial((2 * l) - (2 * k))/(Mathf.Factorial(k)* Mathf.Factorial(l - k) * Mathf.Factorial(l - Mathf.Abs(m) - 2 * k))) * Mathf.Pow(Mathf.Cos(theta), l - Mathf.Abs(m) - 2 * k);
+        }
+        float first_part = (1/Mathf.Pow(2, l)) * Mathf.Sqrt(((2 * l + 1) / 2) * Mathf.Factorial(l - Mathf.Abs(m))/Mathf.Factorial(l + Mathf.Abs(m))) * Mathf.Pow(Mathf.Pow(Mathf.Cos(theta), 2) + 1, Mathf.Abs(m) / 2);
+        return (p_sum * first_part);
+    }
+
+    float get_normalization_constant(float zeta, int n)
+    {
+        float first_part = Mathf.Pow(2 * zeta, n);
+        float second_part = Mathf.Sqrt((2 * zeta) / Mathf.Factorial(2 * n));
+        return (first_part * second_part);
     }
 }
